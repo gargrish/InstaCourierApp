b94667927d272589df0707b291342ebf592f49f8
vidurthakral <vidurthakral@gmail.com>
2015-09-01 23:58:55 +0530

    db basic schema

---
+++ src/pojos/UserCardVO.java
@@ -0,0 +1,27 @@
+package pojos;
+
+public class UserCardVO {
+private int cardNumber;
+private int cvv;
+private String expDate;
+public int getCardNumber() {
+	return cardNumber;
+}
+public void setCardNumber(int cardNumber) {
+	this.cardNumber = cardNumber;
+}
+public int getCvv() {
+	return cvv;
+}
+public void setCvv(int cvv) {
+	this.cvv = cvv;
+}
+public String getExpDate() {
+	return expDate;
+}
+public void setExpDate(String expDate) {
+	this.expDate = expDate;
+}
+
+
+}
